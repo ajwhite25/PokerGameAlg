d6caf2f9005293d4fff627870ed5cf421d9653ec
Amiya W <Whiteamiya17@gmail.com>
2025-04-28 12:42:28 -0500

    intial definitions

---
+++ texas_holdem.py
@@ -0,0 +1,21 @@
+import random
+import math
+
+
+def bet():
+    pass
+
+def call():
+    pass
+
+def fold():
+    pass
+
+def raise_act():
+    pass
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
