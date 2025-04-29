9457ca7949dd710dab5086355ba4d5d82877a608
Amiya W <Whiteamiya17@gmail.com>
2025-04-29 15:22:10 -0500

    updated main()

--- texas_holdem.py
+++ texas_holdem.py
@@ -35,7 +35,7 @@ class TexasHoldEm:
         # else: 
         pass
 
-    def call_check(self):
+    def call(self):
         #all operations will be called within call
         #call everytime until first 3 cards come out!
         # if low two pairs just call
@@ -101,45 +101,60 @@ def main():
         game.get_betting_phases = 1
         turn= input("Who is going first? (i/u)")
 
-        while(1):
-            if(turn =='u'):
-                oppponent_action = input("What did the opponent do? ") 
-                game.set_opponent_action = oppponent_action
-                if(oppponent_action=='raised'):
-                     pot = int(input("How much? "))
-                #if they fold we automatically win.\
-                elif(oppponent_action =='fold'):
-                     print(f"I WIN!\n Money Remaining: {amount}. \n\n")
-                
-                game.call_check()
-                turn = 'i'
+        while True:
+            # Played 0: i, PLayed 1: u
+            played = [False, False]
 
-            #do stuff
-            game.bet()
-            turn='u'
+            while not (played[0], played[1]):
+                if(turn =='u') and not played[1]:
+                    oppponent_action = input("What did the opponent do? ") 
+                    game.set_opponent_action = oppponent_action
+                    played[1] = True
+
+                    if(oppponent_action=='raised'):
+                        pot = int(input("How much? "))
+                    #if they fold we automatically win.\
+                    elif(oppponent_action =='fold'):
+                        print(f"I WIN!\n Money Remaining: {amount}. \n\n")
+                        exit()
+                    
+                    # My game is taken next.
+                    turn = 'i'
+
+                elif(turn =='i') and not played[0]:
+                    action = game.bet()
+                    print(action)
+                    played[0]= True
+                    turn='u'
+
             new_phase=input("Next Phase? (2,3,4) ")
+
             match new_phase:
                 case '2':
-                      # The Flop
-                    game.get_betting_phases = 2
+                    # The Flop
+                    game.get_betting_phases = new_phase
                     for c in range (0,3):
                         number = input(f"{c}: Number on card: (A,Q,J,K,10-2) ")
                         symbol = input(f"{c}: Symbol on card: (s,d,h,c) ")
                         current_deal[number] = symbol
-
+                    break
+                
                 case '3':
-                   current_deal[input(f"{c}: Number on card: (A,Q,J,K,10-2) ")] = input(f"{c}: Symbol on card: (s,d,h,c) ")
+                    game.get_betting_phases = new_phase
+                    # The Turn
+                    #fold on turn phase IF we don't see anything we can handle (in array in the phase class)
+                    current_deal[input(f"{c}: Number on card: (A,Q,J,K,10-2) ")] = input(f"{c}: Symbol on card: (s,d,h,c) ")
+                    break
 
                 case '4':
+                    game.get_betting_phases = new_phase
+                    #The River
+                    #Closing!
                     current_deal[input(f"{c}: Number on card: (A,Q,J,K,10-2) ")] = input(f"{c}: Symbol on card: (s,d,h,c) ")
-
-    
+                    #outputs--  tell them what action and by how much. 
+            turn= input("Who is going first? (i/u)")
+        
             # reminder, call everytime until house puts card down
             # raise if and anytime we get two high cards
-            #fold on turn phase IF we don't see anything we can handle (in array in the phase class)
-
-            #array size does not exceed 7
-
-#outputs--  tell them what action and by how much. 
 if __name__ == "__main__":
             main()
